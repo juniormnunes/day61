7138b1aa14e0156850a51e720b131147557fedee
juniormnunes <juniormnunes@hotmail.com>
2026-02-05 05:23:26 +0100

    1

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, ValidationError, InputRequired
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = PasswordField('Password')
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
+    email = StringField(label='Email', validators=[
+        DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
 
-On Windows type:
-python -m pip install -r requirements.txt
+    def validate_email(self, email):
+        if "@" not in email.data or "." not in email.data:
+            raise ValidationError("Nao tem gocciola.")
+        
 
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+    def validate_password(self, password):
+        if len(password.data) < 8:
+            raise ValidationError("Senha muito curta.")
 
 
 app = Flask(__name__)
@@ -29,9 +28,10 @@ app.secret_key = '12345'
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
 
 
